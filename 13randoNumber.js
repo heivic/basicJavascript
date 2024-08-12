54c30e1c2644deaaa65299524f7af299cebf18a6
Fábio Santos <116457022+heivic@users.noreply.github.com>
2024-08-12 18:33:41 -0300

    randomNumber

---
+++ 13randoNumber.js
@@ -0,0 +1,14 @@
+//Gerar um número aleatório entre 1 e 10.
+function aleatorioNumber() {
+    var resultado = Math.floor(Math.random() * (10 - 1 + 1)) + 1
+    console.log(resultado)
+    return
+}
+
+aleatorioNumber()
+/*
+    Material de Estudo:
+    https://www.youtube.com/watch?v=EEyrkoUlqoU
+
+    https://www.youtube.com/watch?v=8xqU_hXb-9k
+*/
