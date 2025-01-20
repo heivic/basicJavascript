cd2deedf45c4136ec0fa912f361e4ae1159be519
Fábio Santos <116457022+heivic@users.noreply.github.com>
2025-01-20 19:57:51 -0300

    2025

---
+++ 20soma1a100par.js
@@ -0,0 +1,7 @@
+var zero = 0
+
+for(i = 0; i <= 100; i += 2){
+    zero += i
+}
+
+console.log("Os número pares de 1 a 100 são: "+ zero)
---
+++ 21soma1a100impar.js
@@ -0,0 +1,7 @@
+var zero = 0
+
+for(i = 1; i <= 100; i += 2){
+    zero += i
+}
+
+console.log(zero)
